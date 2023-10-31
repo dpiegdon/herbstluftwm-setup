fede25ac857f6fe0869ae46dad519835b9eb1f27
David R. Piegdon <dgit@piegdon.de>
2023-10-31 15:21:23 +0000

    adapted config and setup script

--- setup.sh
+++ setup.sh
@@ -1,15 +1,39 @@
-#!/bin/sh
+#!/bin/bash -xe
 
-echo "make sure you installed package  xorg-dev"
-git submodule init
-git submodule update
-cd herbstluftwm
+echo "make sure you installed packages:"
+echo " - xorg-dev"
+echo " - asciidoc"
+echo " - stterm"
+echo " - polybar"
+echo " - suckless-tools (for dmenu_run)"
+
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+git submodule foreach git clean -dfx
+git submodule foreach git reset HEAD --hard
+
+pushd herbstluftwm
 rm -Rf build
 mkdir build
 cd build
 cmake ..
-make -j6
+make -j$(nproc)
+popd
 
-ln -s config $HOME/config/herbstluftwm
-ln -s herbstluftwm/build/herbstclient $HOME/bin/
-ln -s herbstluftwm/build/herbstluftwm $HOME/bin/
+rm -rf $HOME/.config/herbstluftwm
+mkdir -p $HOME/.config/herbstluftwm
+ln -s $(pwd)/config/autostart $HOME/.config/herbstluftwm/autostart
+
+rm -rf $HOME/.config/polybar
+mkdir -p $HOME/.config/polybar
+ln -s $(pwd)/config/polybar.ini $HOME/.config/polybar/config.ini
+
+for BINARY in $(pwd)/bin/*; do
+	NAME=$(basename "$BINARY")
+	TARGET="$HOME/bin/$NAME"
+	rm -f "$TARGET"
+	ln -s "$BINARY" "$TARGET"
+done;
+
+rm -f ~/.xsession
+ln -s $(pwd)/config/xsession ~/.xsession
